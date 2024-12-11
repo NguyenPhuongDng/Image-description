10f61c6115fd7992c408115326bfe93b57280b2a
Anh39 <22022514@vnu.edu.vn>
2024-12-11 23:20:15 +0700

    fine_tune

--- Fine_Tune/util/__init__.py
+++ Fine_Tune/util/__init__.py
@@ -1,8 +1,4 @@
 from .cnn_train_helper import train_eval as cnn_train_eval, save_model
-<<<<<<< HEAD
 from .flickr8k_dataloader import Flickr8kDataset, get_dataloader
 from .train_helper import train_eval
 from .multi_part_model_serialize import load_model_chunks, save_model_chunks
-=======
-from .flickr8k_dataloader import Flickr8kDataset
->>>>>>> 82efd33f869ad4f0223f9076e75d016a67bbecca
