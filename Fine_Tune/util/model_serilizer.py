bbe4dad1125755e285815d30d6a94eb1017c13a2
Anh39 <22022514@vnu.edu.vn>
2024-12-12 19:37:36 +0700

    fix model loading

--- Fine_Tune/util/model_serilizer.py
+++ Fine_Tune/util/model_serilizer.py
@@ -37,7 +37,11 @@ def load_model_chunks(folder_path: str, name: str):
     for file_path in file_paths:
         with open(file_path, 'rb') as file:
             model_bytes += file.read()
-    model = torch.load(io.BytesIO(model_bytes), weights_only=False)
+    temp_file = os.path.join(folder_path, "temp.pt")
+    with open(temp_file, 'wb') as file:
+        file.write(model_bytes)
+    model = torch.load(temp_file, weights_only=False)
+    os.remove(temp_file)
     return model
 
 def save_state_dicts(state_dict, folder_path: str, name: str, max_chunk_size: int):
@@ -45,18 +49,18 @@ def save_state_dicts(state_dict, folder_path: str, name: str, max_chunk_size: in
     if not os.path.exists(folder_path): os.makedirs(folder_path)
     torch.save({"data" : state_dict}, file_path)
     model_size = os.path.getsize(file_path)
-    if model_size > max_chunk_size:
-        with open(file_path, 'rb') as file:
-            model_bytes = file.read()
-        num_chunks = (len(model_bytes) // max_chunk_size) + 1
-        chunk_size = len(model_bytes) // num_chunks
-        for i in range(num_chunks):
-            start_index = i * chunk_size
-            end_index = min((i + 1) * chunk_size, len(model_bytes))
-            chunk_file_name = os.path.join(folder_path, f"{name}{i}.pt")
-            with open(chunk_file_name, 'wb') as file:
-                file.write(model_bytes[start_index:end_index])
-        os.remove(file_path)
+    # if model_size > max_chunk_size:
+    #     with open(file_path, 'rb') as file:
+    #         model_bytes = file.read()
+    #     num_chunks = (len(model_bytes) // max_chunk_size) + 1
+    #     chunk_size = len(model_bytes) // num_chunks
+    #     for i in range(num_chunks):
+    #         start_index = i * chunk_size
+    #         end_index = min((i + 1) * chunk_size, len(model_bytes))
+    #         chunk_file_name = os.path.join(folder_path, f"{name}{i}.pt")
+    #         with open(chunk_file_name, 'wb') as file:
+    #             file.write(model_bytes[start_index:end_index])
+    #     os.remove(file_path)
 def load_state_dicts(folder_path: str, name: str):
     files = os.listdir(folder_path)
     i = 0
